1d52322aafbe974b927dd088dde6241434856698
Megan V <elbowgrrl@gmail.com>
2021-08-04 16:30:49 -0700

    making backup of codebase with all routes to /messages and no routes to /journal

--- app/controllers/journal_controller.rb
+++ app/controllers/journal_controller.rb
@@ -1,9 +1,3 @@
 class JournalController < ApplicationController
 
-  def create
-    @message = Message.new(messages: params['message'])
-    @message.save
-    redirect_to '/messages', status: :ok
-  end  
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/messages', to: 'messages#index', as: 'messages'
 
-  post '/Journal', to: 'journal#create', as: 'journal'
-
-  #get '/Journal', to: 'messages#create', as: 'journal'
-
-  #post '/Journal', to: 'messages#create', as: 'journals'
+  post '/messages', to: 'messages#create', as: 'message'
 
   get '/animals', to: 'animals#index', as: 'animals'
 
