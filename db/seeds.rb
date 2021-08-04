0daea469d50ff965bb553b6642a16c894bb7226b
cyn <cynthiacui98@gmail.com>
2021-08-04 12:23:15 -0700

    img fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,27 +13,27 @@ Message.create!([{
   begin to grow hazy about the eyes, and begin to be over conscious of
   my lungs, I do not mean to have it inferred that I ever go to sea as
   a passenger. ",
-  mood_icon_url: "https://i.imgur.com/LDteTef"
+  mood_icon_url: "https://i.imgur.com/LDteTef.png"
 },
 {
   messages: "Lemon drops halvah biscuit liquorice brownie candy canes danish. Cupcake jelly croissant jujubes marshmallow toffee. Gummi bears pie tart cookie dragée dragée cake liquorice.",
-  mood_icon_url: "https://i.imgur.com/yRy6clh"
+  mood_icon_url: "https://i.imgur.com/yRy6clh.png"
 },
 {
   messages: "Привет! Рада помочь с проектом!",
-  mood_icon_url: "https://i.imgur.com/mPMUoYG"
+  mood_icon_url: "https://i.imgur.com/mPMUoYG.png"
 },
 {
   messages: "Sometimes I wonder what I would say if I had, for a few moments, the attention of the entire world. I like to think that I would say something moving and profound, but I am a bit afraid of speaking in front of a crowd.",
-  mood_icon_url: "https://i.imgur.com/LDteTef"
+  mood_icon_url: "https://i.imgur.com/LDteTef.png"
 },
 {
   messages: "I like to eat soup in the bathtub while my dog plays the violin",
-  mood_icon_url: "https://i.imgur.com/WVNoRDr"
+  mood_icon_url: "https://i.imgur.com/WVNoRDr.png"
 },
 {
   messages: "Motivation is not the complete answer. If you aren't educated, then you'll end up a motivated idiot. It's important to connect curiosity and discovery to your motivation. Keep an open mind as you proceed into your future.",
-  mood_icon_url: "https://i.imgur.com/2G6Cc0q"
+  mood_icon_url: "https://i.imgur.com/2G6Cc0q.png"
 }])
 
 Animal.destroy_all
